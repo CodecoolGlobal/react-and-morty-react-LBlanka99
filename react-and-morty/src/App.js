728f070cc0e2253e7e32cf7c586a396f76b57d45
Losonci Blanka <losonci.blanka@mensa.hu>
2023-01-16 13:10:03 +0100

    deleted files

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
-import { useCharacters, useLocations } from "./api/useData";
 
 function App() {
-  const characters = useCharacters(1);
-  const locations = useLocations(1);
 
   console.log("Characters data: ");
   console.log(characters);
