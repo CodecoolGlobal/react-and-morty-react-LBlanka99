a67193c8dd25fd356d8d3ba161e91716b07e9386
pjung86 <peter.jung.codecool@gmail.com>
2023-01-16 13:29:23 +0100

    add starting kit and logo

--- react-and-morty/src/App.js
+++ react-and-morty/src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-
-  console.log("Characters data: ");
-  console.log(characters);
-  console.log("Locations data: ");
-  console.log(locations);
-
+  
   return <div className="App">Take a look at the console! (F12)</div>;
 }
 
