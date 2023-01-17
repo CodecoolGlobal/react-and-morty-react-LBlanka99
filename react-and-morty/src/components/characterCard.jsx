e8cc5b327560f25752b7cd137482300d933627a1
csekea <113106666+csekea@users.noreply.github.com>
2023-01-17 13:43:28 +0100

    add turning effect and back side to cards

--- react-and-morty/src/components/characterCard.jsx
+++ react-and-morty/src/components/characterCard.jsx
@@ -3,12 +3,23 @@ import React from "react";
 const CharacterCard = ({ character, onClick }) => {
   return (
     <div onClick={onClick} className="character-card">
-      <img src={character.image} alt="pic" />
-      <div className="card-info">
-        Name: {character.name}
-        <br />
-        Species: {character.species}
-        <br />
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <img src={character.image} alt="pic" />
+          <div className="card-info">
+            Name: {character.name}
+            <br />
+            Species: {character.species}
+            <br />
+          </div>
+        </div>
+        <div className="flip-card-back">
+          <h3>Name: {character.name}</h3><br />
+          Gender: {character.gender}<br />
+          Location: {character.location.name}<br />
+          Origin: {character.origin.name}<br />
+          Status: {character.status}
+        </div>
       </div>
     </div>
   );
