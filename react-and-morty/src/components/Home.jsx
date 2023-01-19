7632e558980fa4a80f57135030cf48eb962094cc
pjung86 <peter.jung.codecool@gmail.com>
2023-01-19 13:31:39 +0100

    fine tune toggle switch and card hover CSS

--- react-and-morty/src/components/Home.jsx
+++ react-and-morty/src/components/Home.jsx
@@ -6,14 +6,18 @@ const Home = ({showCharacters, showLocations, handleToggle}) => {
     <div className="home-container">
       <img alt="logo" src={logo} width="750"/>
         <div className="home-buttons">
-        <button className="active-button" onClick= {showCharacters}>Characters</button>
-        <button className="active-button" onClick= {showLocations}>Locations</button>
-        </div>
-        <div id="toggle-box">
-        <label className="switch">Infinite scroll: 
+          <div className="spacer"></div>
+          <div className="load-buttons">
+            <button className="active-button" onClick= {showCharacters}>Characters</button>
+            <button className="active-button" onClick= {showLocations}>Locations</button>
+          </div>
+        <div className="toggle-box">
+          Infinite scroll:
+        <label className="switch"> 
           <input  type="checkbox"/>
           <span onClick={handleToggle} className="slider round"></span>
         </label>
+        </div>
         </div>
       <div className="description">
         <h1>Welcome to the Rick and Morty Library</h1>
